0b4fce032e62619ed5cbb0c50081ca92e24978a0
Zabelnikov <sergeyzabelnikov@jelvix.com>
2016-02-19 18:33:33 +0200

    Added activity

--- app/src/main/java/com/philosophy/mvp/MainActivity.java
+++ app/src/main/java/com/philosophy/mvp/MainActivity.java
@@ -2,13 +2,9 @@ package com.philosophy.mvp;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+
+import com.philosophy.mvp.main.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +12,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.container, new MainFragment())
+                    .commit();
+        }
 
     }
 }
