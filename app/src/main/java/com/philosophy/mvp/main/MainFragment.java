ca30aac360b1f8b0d47d350ef77d50f0efdc85e7
E.Samoilov <jx.samoilov@gmail.com>
2016-02-19 18:53:16 +0200

    refactored mvp

--- app/src/main/java/com/philosophy/mvp/main/MainFragment.java
+++ app/src/main/java/com/philosophy/mvp/main/MainFragment.java
@@ -1,5 +1,12 @@
 package com.philosophy.mvp.main;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.philosophy.mvp.R;
 import com.philosophy.mvp.base.impl.MVPBaseFragment;
 import com.philosophy.mvp.main.mvp.IMainView;
 import com.philosophy.mvp.main.mvp.MainPresenter;
@@ -9,5 +16,11 @@ public class MainFragment extends MVPBaseFragment<IMainView, MainPresenter> impl
     @Override
     public Class<MainPresenter> getMVPPresenterClass() {
         return MainPresenter.class;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.content_main, container, false);
     }
 }
